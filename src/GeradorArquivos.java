af6a6a0493842d3798abdacb9c25c364d73dd499
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-10-07 22:50:49 -0300

    feat: relatório

--- src/GeradorArquivos.java
+++ src/GeradorArquivos.java
@@ -171,7 +171,7 @@ public class GeradorArquivos {
         tam a quantidade de registros que deseja e passar definir o tipo de arquivo
         que pode ser ORDENADO ou BALANCEADO*/
         
-        int TAM = 1000;
+        int TAM = 25000;
         long tempoInicial = System. currentTimeMillis();        
         g.geraArquivo(TAM,TipoArquivo.ALEATORIO);
         long tempoFinal = System. currentTimeMillis();
